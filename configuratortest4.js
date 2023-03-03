62027a512dd9b94ce53755d0b3d6192e4072290c
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-03-03 18:02:08 +0100

    Update and rename configuratortest3.js to configuratortest4.js

--- configuratortest4.js
+++ configuratortest4.js
@@ -1,4 +1,4 @@
-<script>
+
 document.addEventListener("DOMContentLoaded", function() {
   var teachingLocationRadios = document.querySelectorAll('input[name="teachingLocation"]');
   var teachingLocationRadios1 = document.querySelectorAll('input[name="unit45"]');
@@ -201,4 +201,4 @@ function calculation() {
   
 
 });
-</script>
+
