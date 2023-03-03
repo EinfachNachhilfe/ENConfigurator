ab1fcd899184665c509ccf4996166414784cf78e
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-03-03 18:57:53 +0100

    Update and rename codetestseite38.js to codetestseite39.js

--- codetestseite39.js
+++ codetestseite39.js
@@ -1,5 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
+	var teachingLocationRadios = document.querySelectorAll('input[name="radiotest1"]');
+        var teachingLocationRadios1 = document.querySelectorAll('input[name="radiotest2"]');
         var mycustomradiobox1 = document.querySelectorAll('input[name="teachingLocation"]');
 	var mycustomradiobox2 = document.querySelectorAll('input[name="unit45"]');
 	var mycustomradiobox3 = document.querySelectorAll('input[name="unit90"]');
