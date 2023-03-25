b7bc6ca29ed2698791e263551821cc3bed239695
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-03-25 14:24:42 +0100

    Update and rename application4.js to application5.js

---
+++ application5.js
@@ -0,0 +1,19 @@
+<script>
+  $(document).ready(function(){
+    $('.div-add-subject').click(function(){
+      if ($('#div-subject2').is(':hidden')) {
+        $('#div-subject2').show();
+      } else if ($('#div-subject2').is(':visible')) {
+        $('#div-subject3').show();
+      }
+    });
+
+    $('#image-subject2').click(function(){
+      $('#subject-2').prop('selectedIndex',0);
+      $('#classTo-2').prop('selectedIndex',0);
+      $('#classFrom-2').prop('selectedIndex',0);
+      $('#div-subject2').hide();
+      $('#div-subject3').hide();
+    });
+  });
+</script>
