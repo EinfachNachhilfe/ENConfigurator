76cc4938372b36b316d99dfb91c26e8584c12d57
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-03-25 15:10:33 +0100

    Update and rename application19.js to application20.js

--- application20.js
+++ application20.js
@@ -5,11 +5,7 @@ $(document).ready(function(){
       if ($('#div-subject2').is(':hidden')) {
         $('#div-subject2').show();
       } else if ($('#div-subject2').is(':visible')) {
-        $('#div-subject3').show(); 
-      } else if ($('#div-subject3').is(':visible')) {
-        $('#div-subject5').show();  
-      } else if ($('#div-subject5').is(':visible')) {
-        $('#div-subject6').show();   
+        $('#div-subject4').show(); 
       }
       
     });
