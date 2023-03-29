75408c2d5623e55abb2f88c013efe2e64d69fcf2
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-03-29 16:11:55 +0200

    Update and rename nachhilfe-nehmen-configurator-overlay_hide2.js to nachhilfe-nehmen-configurator-overlay_hide3.js

--- nachhilfe-nehmen-configurator-overlay_hide3.js
+++ nachhilfe-nehmen-configurator-overlay_hide3.js
@@ -1,6 +1,5 @@
 
-$(document).ready(function() {
-     
+$('#nachhilfe-nehmen-configurator-overlay').show();    
 $('#nachhilfe-nehmen-configurator-overlay-start').click(function() {
         if ($('#nachhilfe-nehmen-configurator-overlay').is(':visible')) {    
             $('#nachhilfe-nehmen-configurator-overlay').hide();
