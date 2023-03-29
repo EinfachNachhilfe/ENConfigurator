67258a9bb2e39e7f7fe81500cdc216ddac40e44d
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-03-29 15:56:10 +0200

    Create nachhilfe-nehmen-configurator-overlay_hide.js

---
+++ nachhilfe-nehmen-configurator-overlay_hide.js
@@ -0,0 +1,6 @@
+
+    $('#nachhilfe-nehmen-configurator-overlay-start').click(function() {
+        if ($('#nachhilfe-nehmen-configurator-overlay').is(':visible')) {    
+            $('#nachhilfe-nehmen-configurator-overlay').hide();
+        }
+    });
