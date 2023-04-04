94c06aeb670591c10e48e7d42f303d18c3e9863b
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-04-04 17:05:42 +0200

    Update and rename configurator-step63.js to configurator-step64.js

--- configurator-step64.js
+++ configurator-step64.js
@@ -245,7 +245,6 @@ function checkTeachingLocationSelected() {
       }
     });
 
-    isAnyCheckboxSelected3 = false;
     
     if (isRuntimeSelected) {
       nextBtn.classList.remove('disabled');
