00ece53a45a5a4f055021da0993ea57882421fd8
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-04-04 13:18:58 +0200

    Update and rename configurator-step33.js to configurator-step34.js

--- configurator-step34.js
+++ configurator-step34.js
@@ -128,7 +128,7 @@ function checkTeachingLocationSelected() {
   function checkRuntimeSelected() {
     runtimeRadios.forEach(function(radio) {
       if (radio.checked) {
-        if (radio.name === 'runtime-3Unit') {
+        if (radio.name === 'runtime-1Unit') {
           isRuntimeSelected = true;
           if (radio.value === '1') { 
               tutor1.style.display = 'block';
@@ -159,6 +159,22 @@ function checkTeachingLocationSelected() {
              tutor1.style.display = 'none';
              tutor2.style.display = 'block';
              tutor3.style.display = 'none';
+          }
+          
+                 isRuntimeSelected = true;
+        } else if (radio.name === 'runtime-3Unit') {
+          if (radio.value === '1') {
+             tutor1.style.display = 'none';
+             tutor2.style.display = 'none';
+             tutor3.style.display = 'block';
+          } else if (radio.value === '2') {
+             tutor1.style.display = 'none';
+             tutor2.style.display = 'none';
+             tutor3.style.display = 'block';
+          } else if (radio.value === '3') {
+             tutor1.style.display = 'none';
+             tutor2.style.display = 'none';
+             tutor3.style.display = 'block';
           }
           
           
