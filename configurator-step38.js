d9dba56b44fc3d822475fd66770280a09435f1ea
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-04-04 13:32:03 +0200

    Update and rename configurator-step37.js to configurator-step38.js

--- configurator-step38.js
+++ configurator-step38.js
@@ -125,61 +125,77 @@ function checkTeachingLocationSelected() {
     }
   }
 
-function checkRuntimeSelected() {
-  var isRuntimeSelected = false;
+  function checkRuntimeSelected() {
+    runtimeRadios.forEach(function(radio) {
+      if (radio.checked) {
+        if (radio.name === 'runtime-1Unit') {
+          isRuntimeSelected = true;
+          if (radio.value === '1') { 
+              tutor1.style.display = 'block';
+              tutor2.style.display = 'none';
+              tutor3.style.display = 'none';
+          } else if (radio.value === '2') {
+              tutor1.style.display = 'block';
+              tutor2.style.display = 'none';
+              tutor3.style.display = 'none';
+          } else if (radio.value === '3') {
+              tutor1.style.display = 'block';
+              tutor2.style.display = 'none';
+              tutor3.style.display = 'none';
+          }
 
-  runtimeRadios.forEach(function(radio) {
-    if (radio.checked) {
-      isRuntimeSelected = true;
-
-      if (radio.name === 'runtime-1Unit') {
-        if (radio.value === '1') { 
-          tutor1.style.display = 'block';
-          tutor2.style.display = 'none';
-          tutor3.style.display = 'none';
-        } else if (radio.value === '2') {
-          tutor1.style.display = 'block';
-          tutor2.style.display = 'none';
-          tutor3.style.display = 'none';
-        } else if (radio.value === '3') {
-          tutor1.style.display = 'block';
-          tutor2.style.display = 'none';
-          tutor3.style.display = 'none';
-        }
-      } else if (radio.name === 'runtime-2Unit') {
-        if (radio.value === '1') {
-          tutor1.style.display = 'none';
-          tutor2.style.display = 'block';
-          tutor3.style.display = 'none';
-        } else if (radio.value === '2') {
-          tutor1.style.display = 'none';
-          tutor2.style.display = 'block';
-          tutor3.style.display = 'none';
-        } else if (radio.value === '3') {
-          tutor1.style.display = 'none';
-          tutor2.style.display = 'block';
-          tutor3.style.display = 'none';
-        }
-      } else if (radio.name === 'runtime-3Unit') {
-        if (radio.value === '1') {
-          tutor1.style.display = 'none';
-          tutor2.style.display = 'none';
-          tutor3.style.display = 'block';
-        } else if (radio.value === '2') {
-          tutor1.style.display = 'none';
-          tutor2.style.display = 'none';
-          tutor3.style.display = 'block';
-        } else if (radio.value === '3') {
-          tutor1.style.display = 'none';
-          tutor2.style.display = 'none';
-          tutor3.style.display = 'block';
+          
+          isRuntimeSelected = true;
+        } else if (radio.name === 'runtime-2Unit') {
+          if (radio.value === '1') {
+             tutor1.style.display = 'none';
+             tutor2.style.display = 'block';
+             tutor3.style.display = 'none';
+          } else if (radio.value === '2') {
+             tutor1.style.display = 'none';
+             tutor2.style.display = 'block';
+             tutor3.style.display = 'none';
+          } else if (radio.value === '3') {
+             tutor1.style.display = 'none';
+             tutor2.style.display = 'block';
+             tutor3.style.display = 'none';
+          }
+          
+                 isRuntimeSelected = true;
+        } else if (radio.name === 'runtime-3Unit') {
+          if (radio.value === '1') {
+             tutor1.style.display = 'none';
+             tutor2.style.display = 'none';
+             tutor3.style.display = 'block';
+          } else if (radio.value === '2') {
+             tutor1.style.display = 'none';
+             tutor2.style.display = 'none';
+             tutor3.style.display = 'block';
+          } else if (radio.value === '3') {
+             tutor1.style.display = 'none';
+             tutor2.style.display = 'none';
+             tutor3.style.display = 'block';
+         
+         }
+          
+             isRuntimeSelected = true;
+        } else if (radio.name === 'runtime-4Unit') {
+          if (radio.value === '1') {
+             tutor1.style.display = 'none';
+             tutor2.style.display = 'none';
+             tutor3.style.display = 'none';
+          } else if (radio.value === '2') {
+             tutor1.style.display = 'none';
+             tutor2.style.display = 'none';
+             tutor3.style.display = 'none';
+          } else if (radio.value === '3') {
+             tutor1.style.display = 'none';
+             tutor2.style.display = 'none';
+             tutor3.style.display = 'none';
+          }
         }
       }
-    }
-  });
-
-  return isRuntimeSelected;
-}
+    });
     
     if (isRuntimeSelected) {
       nextBtn.classList.remove('disabled');
