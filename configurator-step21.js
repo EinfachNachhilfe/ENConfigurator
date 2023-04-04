50198d9531358b2a04f90e69285015033236cc91
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-04-04 11:09:45 +0200

    Update and rename configurator-step20.js to configurator-step21.js

--- configurator-step21.js
+++ configurator-step21.js
@@ -139,38 +139,6 @@ function checkTeachingLocationSelected() {
               tutor2.style.display = 'none';
               tutor3.style.display = 'none';
           }
-
-          
-          isRuntimeSelected = true;
-        } else if (radio.name === 'runtime-2Unit') {
-          if (radio.value === '1') {
-             tutor1.style.display = 'none';
-             tutor2.style.display = 'block';
-             tutor3.style.display = 'none';
-          } else if (radio.value === '2') {
-             tutor1.style.display = 'none';
-             tutor2.style.display = 'block';
-             tutor3.style.display = 'none';
-          } else if (radio.value === '3') {
-             tutor1.style.display = 'none';
-             tutor2.style.display = 'block';
-             tutor3.style.display = 'none';
-          }
-          isRuntimeSelected = true;
-        } else if (radio.name === 'runtime-1Unit') {
-          if (radio.value === '1') {
-             tutor1.style.display = 'none';
-             tutor2.style.display = 'none';
-             tutor3.style.display = 'block';
-          } else if (radio.value === '2') {
-             tutor1.style.display = 'none';
-             tutor2.style.display = 'none';
-             tutor3.style.display = 'block';
-          } else if (radio.value === '3') {
-             tutor1.style.display = 'none';
-             tutor2.style.display = 'none';
-             tutor3.style.display = 'block';
-          }
         }
       }
     });
