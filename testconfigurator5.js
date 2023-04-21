6f88b082c3232c1f6db3756d9c4d0dca75cb9e3f
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-04-21 12:14:07 +0200

    Update and rename testconfigurator4.js to testconfigurator5.js

---
+++ testconfigurator5.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+	var mycustomradiobox2 = document.querySelectorAll('input[name="unit45"]');
+
+	var totalBudget = 10;
+	var lifetimeValue;
+        var totalBudget1 = totalBudget;
+    
+    function updateLifetimeValue() {
+        mycustomradiobox2.forEach(function(radio) {
+            if (radio.checked) {
+                if (radio.value == '1') {
+                    lifetimeValue = totalBudget1 * 1 * 4.33;
+                } else if (radio.value == '2') {
+                    lifetimeValue = totalBudget1 * 2 * 4.33;
+                } else if (radio.value == '3') {
+                    lifetimeValue = totalBudget1 * 3 * 4.33;
+                }
+            }
+        });
+    
+        $('.send-lifetimeValue').val(lifetimeValue);
+        $("[bloc=budget1]").text(totalBudget1);
+    }
+
+	
