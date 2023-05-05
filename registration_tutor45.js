d6377c3bf5a9f9ec7990ac77e8553aadadd975d3
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-05-05 14:45:42 +0200

    Update and rename registration_tutor44.js to registration_tutor45.js

--- registration_tutor45.js
+++ registration_tutor45.js
@@ -117,15 +117,18 @@ function checkAllFieldsFilled3() {
 
 isUploadInputChangeV1.addEventListener('click', function() {
   certificateOfEnrollmentTutor.value = '';
+    isAllFieldsFilled3 = false;
   checkAllFieldsFilled3();
 });
 
 certificateOfEnrollmentTutor.addEventListener('change', function() {
+    isAllFieldsFilled3 = false;
   checkAllFieldsFilled3();
 });
 
 requiredFields3.forEach(function(input) {
   input.addEventListener('input', function() {
+      isAllFieldsFilled3 = false;
     checkAllFieldsFilled3();
   });
 });
