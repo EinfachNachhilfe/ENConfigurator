4f3396f3b1f1518fd41102410c8eac2ba558a514
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-05-08 16:51:45 +0200

    Update and rename registration_customer-step5.js to registration_customer-step6.js

--- registration_customer-step6.js
+++ registration_customer-step6.js
@@ -24,6 +24,15 @@ var isAnyGenderRadioSelected2 = false;
 var isAnyGenderRadioSelected3 = false;
 
 
+const divBillingAddress = document.getElementById("content_billing-address");
+const billingAddressPayable = document.getElementsByName("trigger_billing-address");
+const streetNamePayable = document.getElementById("street-name_billing-address");
+const houseNumberPayable = document.getElementById("house-number_billing-address");
+const ZIPCodePayable = document.getElementById("zip-code_billing-address");
+const cityNamePayable = document.getElementById("city-name_billing-address");
+divBillingAddress.style.display = "none";
+
+
 
 document.addEventListener("DOMContentLoaded", function() {
 checkAllFieldsFilled1();
@@ -91,6 +100,8 @@ function checkAllFieldsFilled2() {
   } else {
     nextBtn.classList.add('disabled');
   } 
+
+  billingAdress()
 }
 requiredFields2.forEach(function(input) {
   input.addEventListener('input', function() {
@@ -243,3 +254,31 @@ backBtn.addEventListener('click', function() {
 
 });
 });
+
+
+  
+  
+  
+function billingAdress() {
+  billingAddressPayable.forEach(function(input) {
+    input.addEventListener("change", function() {
+      if (input.value === "1") {
+        divBillingAddress.style.display = "none";
+        streetNamePayable.value = "";
+        houseNumberPayable.value = "";
+        ZIPCodePayable.value = "";
+        cityNamePayable.value = "";
+        streetNamePayable.required = false;
+        houseNumberPayable.required = false;
+        ZIPCodePayable.required = false;
+        cityNamePayable.required = false;
+      } else if (input.value === "2") {
+        divBillingAddress.style.display = "block";
+        streetNamePayable.required = true;
+        houseNumberPayable.required = true;
+        ZIPCodePayable.required = true;
+        cityNamePayable.required = true;
+      }
+    });
+  });
+}
