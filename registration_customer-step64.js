05ba7dba26d71c20d8175a893685e6794b89f031
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-05-09 15:18:10 +0200

    Update and rename registration_customer-step63.js to registration_customer-step64.js

--- registration_customer-step64.js
+++ registration_customer-step64.js
@@ -157,7 +157,10 @@ requiredFields2.forEach(function(input) {
 });
 
 
-
+        streetNamePayable.required = true;
+        houseNumberPayable.required = true;
+        ZIPCodePayable.required = true;
+        cityNamePayable.required = true;
 
 function checkAllFieldsFilled3() {
   var filledFields3 = 0;
@@ -204,10 +207,6 @@ function checkAllFieldsFilled3() {
         cityNamePayable.required = false;
       } else if (input.value === "2") {
         divBillingAddress.style.display = "block";
-        streetNamePayable.required = true;
-        houseNumberPayable.required = true;
-        ZIPCodePayable.required = true;
-        cityNamePayable.required = true;
       }
     });
   });
