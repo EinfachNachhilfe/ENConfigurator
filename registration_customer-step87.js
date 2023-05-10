d9d26a59f0beff1a2add911cb041cb397f501b48
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-05-10 10:13:08 +0200

    Update and rename registration_customer-step86.js to registration_customer-step87.js

--- registration_customer-step87.js
+++ registration_customer-step87.js
@@ -245,14 +245,6 @@ requiredFields3.forEach(function(input) {
   });
 });
   
-  billingAddressFields.forEach(function(input) {
-  input.addEventListener('input', function() {
-    isAllFieldsFilled3 = false;
-    checkAllFieldsFilled3();
-   
-  });
-});
-  
     genderRadios2.forEach(function(radio) {
   radio.addEventListener('change', function() {
     isAnyGenderRadioSelected2 = false;
