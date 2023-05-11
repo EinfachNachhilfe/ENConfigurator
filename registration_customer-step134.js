8fdf079be770a73824873d8fa783e0ece98057e3
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-05-11 09:21:57 +0200

    Update and rename registration_customer-step133.js to registration_customer-step134.js

--- registration_customer-step134.js
+++ registration_customer-step134.js
@@ -168,7 +168,7 @@ function checkAllFieldsFilled3() {
   var filledFields3 = 0;
   var isBillingAddressRequired = false;
   var filledBillingFields = 0;
-  
+
   requiredFields3.forEach(function (field) {
     if (field.value && field.checkValidity()) {
       filledFields3++;
@@ -185,30 +185,39 @@ function checkAllFieldsFilled3() {
     }
   });
 
-  genderRadios3.forEach(function(radio) {
+  genderRadios3.forEach(function (radio) {
     if (radio.checked) {
       isAnyGenderRadioSelected3 = true;
       if (radio.value === "2") {
         isBillingAddressRequired = true;
-        if (
-          streetNamePayable.value && streetNamePayable.checkValidity() &&
-          houseNumberPayable.value && houseNumberPayable.checkValidity() &&
-          ZIPCodePayable.value && ZIPCodePayable.checkValidity() &&
-          cityNamePayable.value && cityNamePayable.checkValidity()
-        ) {
-          filledBillingFields = 4;
-        }
       }
     }
   });
 
+  if (isBillingAddressRequired) {
+    if (
+      streetNamePayable.value && streetNamePayable.checkValidity() &&
+      houseNumberPayable.value && houseNumberPayable.checkValidity() &&
+      ZIPCodePayable.value && ZIPCodePayable.checkValidity() &&
+      cityNamePayable.value && cityNamePayable.checkValidity()
+    ) {
+      filledBillingFields = 4;
+    }
+  } else {
+    filledBillingFields = 4;
+  }
+
   if (
-    isAllFieldsFilled3 && isAnyGenderRadioSelected2 && isAnyGenderRadioSelected3 && (!isBillingAddressRequired || (isBillingAddressRequired && filledBillingFields === 4))
+    isAllFieldsFilled3 &&
+    isAnyGenderRadioSelected2 &&
+    isAnyGenderRadioSelected3 &&
+    filledBillingFields === 4
   ) {
     nextBtn.classList.remove('disabled');
   } else {
     nextBtn.classList.add('disabled');
   }
+}
   
   billingAddressPayable.forEach(function(input) {
     input.addEventListener("change", function() {
