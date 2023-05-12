2523381db428a24d57cd826fa95398631c815c13
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-05-12 11:28:29 +0200

    Update and rename custom_form-input-phone7.js to custom_form-input-phone8.js

---
+++ custom_form-input-phone8.js
@@ -0,0 +1,21 @@
+
+var selectedOption = "";
+
+function updateInputValue() {
+  var selectElement = document.getElementById("custom_form-input-is-select-input");
+  selectedOption = selectElement.options[selectElement.selectedIndex].value;
+  document.getElementById("phone-number_payable").value = selectedOption;
+}
+
+document.getElementById("custom_form-input-is-select-input").addEventListener("change", function() {
+  updateInputValue();
+});
+
+document.getElementById("phone-number_payable").addEventListener("input", function() {
+  if (this.value.length < selectedOption.length) {
+    this.value = selectedOption;
+  }
+});
+
+// Aufrufen der Funktion beim Laden der Seite
+window.onload = updateInputValue;
