07c95d0247e94b4a1ad0ffbb4529a4a9a7e47e39
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-05-12 18:02:31 +0200

    Update and rename registration_customer-step225.js to registration_customer-step226.js

--- registration_customer-step226.js
+++ registration_customer-step226.js
@@ -170,7 +170,7 @@ document.getElementById("phone-number_payable").addEventListener("input", functi
   }
 });
 
-function checkInput() {
+function checkInput2() {
   const input2 = document.getElementById('phone-number_payable');
   if (input2.value.substring(0, selectedOption.length) !== selectedOption) {
     input2.value = selectedOption;
