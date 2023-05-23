ee691c488ce3bf9de66b8bd42e84781684f9d538
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-05-23 17:09:14 +0200

    Update and rename application_step15.js to application_step16.js

--- application_step16.js
+++ application_step16.js
@@ -3,20 +3,32 @@ document.addEventListener("DOMContentLoaded", function() {
 
   const classFromSelect = document.getElementById("class-from-1_tutor");
   const classToSelect = document.getElementById("class-to-1_tutor");
-  classFromSelect.addEventListener("change", function() {
-    const selectedValue = parseInt(classFromSelect.value);
 
-    // Optionen im 'class-to-1_tutor'-Select-Feld ausblenden oder einblenden
+  classFromSelect.addEventListener("change", function() {
+    const selectedFromValue = parseInt(classFromSelect.value);
+    const selectedToValue = parseInt(classToSelect.value);
+
     for (let i = 0; i < classToSelect.options.length; i++) {
       const optionValue = parseInt(classToSelect.options[i].value);
 
-      if (optionValue <= selectedValue) {
+      if (optionValue <= selectedFromValue) {
         classToSelect.options[i].style.display = "none";
       } else {
         classToSelect.options[i].style.display = "block";
       }
     }
   });
+
+  classToSelect.addEventListener("change", function() {
+    const selectedFromValue = parseInt(classFromSelect.value);
+    const selectedToValue = parseInt(classToSelect.value);
+
+    if (selectedFromValue > selectedToValue) {
+      classFromSelect.value = selectedToValue.toString();
+    }
+  });
+
+  
 
   const componentsubject2Tutor = document.getElementById("component-subject-2_tutor");
   const componentsubject3Tutor = document.getElementById("component-subject-3_tutor");
