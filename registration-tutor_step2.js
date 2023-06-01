1ea1ce8b2bfb590b91ccd7f991afaaa3c3651ee3
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-06-01 14:04:13 +0200

    Update and rename registration-tutor_step1.js to registration-tutor_step2.js

--- registration-tutor_step2.js
+++ registration-tutor_step2.js
@@ -82,7 +82,7 @@ function checkInput() {
 var ValidationInputFieldIban1 = document.querySelector('input[name="iban_tutor"]');
 
 function applyValidation3(inputElement3) {
-  inputElement3.setAttribute('pattern', '^DE[0-9]{22}$');
+  inputElement3.setAttribute('pattern', '^DE[0-9]{20}$');
   inputElement3.setAttribute('maxlength', '22');
 
   inputElement3.addEventListener('change', function() {
