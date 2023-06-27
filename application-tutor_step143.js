b5b3aec2f3a3ed9d2444b16b7545b81da3f7251a
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-06-27 16:49:32 +0200

    Update and rename application-tutor_step142.js to application-tutor_step143.js

--- application-tutor_step143.js
+++ application-tutor_step143.js
@@ -40,19 +40,19 @@ document.addEventListener("DOMContentLoaded", function() {
     function updateInputValue() {
       var selectElement = document.getElementById("custom_form-input-is-select-input");
       selectedOption = selectElement.options[selectElement.selectedIndex].value;
-      document.getElementById("phone-number_payable").value = selectedOption;
+      document.getElementById("phone-number_tutor").value = selectedOption;
     }
     document.getElementById("custom_form-input-is-select-input").addEventListener("change", function() {
       updateInputValue();
     });
-    document.getElementById("phone-number_payable").addEventListener("input", function() {
+    document.getElementById("phone-number_tutor").addEventListener("input", function() {
       if (this.value.length < selectedOption.length) {
         this.value = selectedOption;
       }
     });
     updateInputValue();
     function checkInput2() {
-      const input2 = document.getElementById('phone-number_payable');
+      const input2 = document.getElementById('phone-number_tutor');
       if (input2.value.substring(0, selectedOption.length) !== selectedOption) {
         input2.value = selectedOption;
         input2.setSelectionRange(selectedOption.length, selectedOption.length);
@@ -62,7 +62,7 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }
     document.addEventListener('DOMContentLoaded', (event) => {
-      const input2 = document.getElementById('phone-number_payable');
+      const input2 = document.getElementById('phone-number_tutor');
       input2.value = selectedOption;
       input2.addEventListener('input', checkInput2);
       updateInputValue();
