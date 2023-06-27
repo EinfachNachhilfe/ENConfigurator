51d6ca7e8e062e9a93a611180b57cb694e07aadb
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-06-27 17:39:45 +0200

    Update and rename application-tutor_step151.js to application-tutor_step152.js

--- application-tutor_step152.js
+++ application-tutor_step152.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const inputValidationCityNameTutor = document.getElementById('city-name_tutor');
     const inputValidationBdayTutor = document.getElementById('bday-tutor');
     const inputValidationEmailTutor = document.getElementById('email_tutor');
-    const inputValidationPhoneNumberTutor = document.getElementById('phone-number_tutor');
+    const inputValidationPhoneNumberTutor = document.getElementById('custom_form-input-is-select-input');
     const inputValidationInstitutionTutor = document.getElementById('institution_tutor');
     const inputValidationCourseOfStudyTutor = document.getElementById('course-of-study_tutor');
     const inputValidationSemesterTutor = document.getElementById('semester_tutor');
