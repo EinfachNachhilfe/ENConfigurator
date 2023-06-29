117e313168bba188be56622d98ee7fea0a80fae2
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-06-29 15:18:55 +0200

    Update and rename request-customer_configurator2.js to request-customer_configurator3.js

--- request-customer_configurator3.js
+++ request-customer_configurator3.js
@@ -1674,7 +1674,18 @@ document.addEventListener("DOMContentLoaded", function() {
                   }
                 });
                
-           
+                var now = new Date();
+                var year = now.getFullYear();
+                var month = (now.getMonth() + 1).toString().padStart(2, '0');
+                var day = now.getDate().toString().padStart(2, '0');
+                var hours = now.getHours().toString().padStart(2, '0');
+                var minutes = now.getMinutes().toString().padStart(2, '0');
+                var seconds = now.getSeconds().toString().padStart(2, '0');
+                
+                var currentDateTime = year + month + day + '-' + hours + minutes + seconds;
+                var referenz = codegenerator + currentDateTime;
+                
+                $('.customer-referenz').val(referenz);
               
 
 		totalBudget = totalBudget.toFixed(2).replace(".", ",");
