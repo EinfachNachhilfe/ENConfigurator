5cfe43fe5ffcee3a8d1b4eb67a48c26306c2d1a0
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-06-30 12:31:28 +0200

    Update and rename request-customer_configurator4.js to request-customer_configurator5.js

--- request-customer_configurator5.js
+++ request-customer_configurator5.js
@@ -1686,6 +1686,10 @@ document.addEventListener("DOMContentLoaded", function() {
                 var referenz = codegenerator + currentDateTime;
                 
                 $('.customer-referenz').val(referenz);
+
+		var successUrl = "https://www.einfach-nachhilfe.com/nachhilfe-anfragen-danke?referenz=" + encodeURIComponent(referenz);
+                $('#request_customer').attr('action', successUrl);
+
               
 
 		totalBudget = totalBudget.toFixed(2).replace(".", ",");
