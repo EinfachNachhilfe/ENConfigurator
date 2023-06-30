3fc7b9eb54a6bbf280b084053c69556facfe7031
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-06-30 14:45:49 +0200

    Update and rename request-customer_configurator11.js to request-customer_configurator12.js

--- request-customer_configurator12.js
+++ request-customer_configurator12.js
@@ -1691,8 +1691,7 @@ document.addEventListener("DOMContentLoaded", function() {
 		// verborgenes Feld hinzufügen
 
 		
-		var successUrl = "https://einfach-nachhilfe.com/nachhilfe-anfragen-danke?referenz=" + encodeURIComponent(referenz);
-                $('#request_customer').attr('action', successUrl);
+
 
               
 
