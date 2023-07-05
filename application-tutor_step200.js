672034f5af4b6d5ac59690b66883912d15460329
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-07-05 12:24:04 +0200

    Update and rename application-tutor_step199.js to application-tutor_step200.js

--- application-tutor_step200.js
+++ application-tutor_step200.js
@@ -177,6 +177,7 @@ function applyValidationSelect(selectElement, emptyErrorMsg) {
       shakeOnInvalid(selectElement);
     } else {
      errorMessageElement.innerHTML = ''; // Clears the error message
+      errorMessageElement.style.fontSize = '0.75rem';
      errorMessageElement.innerHTML = '&nbsp;'; // Insert a non-breaking space
       selectElement.style.borderColor = '#589b32';
       selectElement.style.borderWidth = '1.5px';
