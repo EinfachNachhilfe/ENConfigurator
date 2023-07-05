900dc603ff2ab9b44221749a8d11859500297a7c
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-07-05 14:15:11 +0200

    Update and rename application-tutor_step201.js to application-tutor_step202.js

--- application-tutor_step202.js
+++ application-tutor_step202.js
@@ -176,12 +176,13 @@ function applyValidationSelect(selectElement, emptyErrorMsg) {
       inValidImage.style.display = 'block';
       shakeOnInvalid(selectElement);
     } else {
-     errorMessageElement.innerHTML = ''; // Clears the error message
+      errorMessageElement.innerHTML = ''; // Clears the error message
       selectElement.style.borderColor = '#589b32';
       selectElement.style.borderWidth = '1.5px';
       validImage.style.display = 'block';
       inValidImage.style.display = 'none';
       errorMessageElement.style.display = 'none';
+      shakeOnInvalid(selectElement);
     }
   });
 }
