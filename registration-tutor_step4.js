b48f07b9c07f932f50df4110d7f65ba13cc33ed1
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-07-07 13:56:42 +0200

    Update registration-tutor_step4.js

--- registration-tutor_step4.js
+++ registration-tutor_step4.js
@@ -33,7 +33,6 @@ document.addEventListener("keydown", function(event) {
 
 
 
-
 //start Validation IBAN
 document.addEventListener('DOMContentLoaded', (event) => {
   const input = document.getElementById('iban_tutor');
@@ -51,6 +50,83 @@ function checkInput() {
     input.value = 'DE' + input.value.substring(2).replace(/\D/g, '');
   }
 }
+
+//end Validation IBAN
+
+  //start inputfield validation
+  function applyValidation(inputElement, emptyErrorMsg, invalidErrorMsg, pattern = null) {
+  let validImage = inputElement.parentNode.querySelector('.form_input-valid-image');
+  let inValidImage = inputElement.parentNode.querySelector('.form_input-invalid-image');
+  const errorMessageElement = document.createElement('span');        
+
+  if (pattern !== null) {
+    inputElement.setAttribute('pattern', pattern);
+  }
+    errorMessageElement.id = 'error_message';
+    errorMessageElement.style.color = '#9d367a';
+    errorMessageElement.style.display = 'none';
+    errorMessageElement.style.marginTop = '-0.625rem';
+    errorMessageElement.style.fontFamily = 'Roboto, sans-serif';
+    errorMessageElement.style.fontSize = '0.8rem';
+    inputElement.parentNode.insertBefore(errorMessageElement, inputElement.nextSibling);
+
+  
+
+    inputElement.addEventListener("change", function() {
+      if (inputElement.value.trim() === '') {
+        errorMessageElement.innerHTML = emptyErrorMsg;
+        errorMessageElement.style.display = 'block';
+        inputElement.style.borderColor = '#9e367a';
+        inputElement.style.borderWidth = '1.5px';
+        validImage.style.display = 'none';
+        inValidImage.style.display = 'block';
+        shakeOnInvalid(inputElement);
+      } else if (inputElement.checkValidity()) {
+        inputElement.style.borderColor = '#589b32';
+        inputElement.style.borderWidth = '1.5px';
+        validImage.style.display = 'block';
+        inValidImage.style.display = 'none';
+        errorMessageElement.style.display = 'none';
+      } else {
+        errorMessageElement.innerHTML = invalidErrorMsg;
+        errorMessageElement.style.display = 'block';
+        inputElement.style.borderColor = '#9e367a';
+        inputElement.style.borderWidth = '1.5px';
+        validImage.style.display = 'none';
+        inValidImage.style.display = 'block';
+        shakeOnInvalid(inputElement);
+      }
+    });
+  }
+
+applyValidation(inputValidationFirstNameTutor, 'Dieses Feld muss ausgefüllt werden.', 'Ungültige Eingabe.', '^[A-Za-z ]+$');
+applyValidation(inputValidationSecondNameTutor, 'Dieses Feld muss ausgefüllt werden.', 'Ungültige Eingabe.', '^[A-Za-z ]+$');
+
+applyValidation(inputValidationStreetNameTutor, 'Dieses Feld muss ausgefüllt werden.', 'Ungültige Eingabe.');
+applyValidation(inputValidationHouseNumberTutor, 'Dieses Feld muss ausgefüllt werden.', 'Ungültige Eingabe.');
+
+  function validateOnButtonClick(inputElement, step) {
+    nextBtn.addEventListener('click', function() {
+      if (window.getComputedStyle(step, null).display === "block" && inputElement.value.trim() === '') {
+        let errorMessageElement = inputElement.parentNode.querySelector('#error_message');
+        let validImage = inputElement.parentNode.querySelector('.form_input-valid-image');
+        let inValidImage = inputElement.parentNode.querySelector('.form_input-invalid-image');
+  
+        errorMessageElement.innerHTML = 'Dieses Feld muss ausgefüllt werden.';
+        errorMessageElement.style.display = 'block';
+        inputElement.style.borderColor = '#9e367a'; // Set border color to red
+        inputElement.style.borderWidth = '1.5px'; // Set border width to 1.5px
+        validImage.style.display = 'none';
+        inValidImage.style.display = 'block';
+        shakeOnInvalid(inputElement);
+      }
+    });
+  }
+  
+  // Anwenden der Funktion auf mehrere Eingabefelder:
+  validateOnButtonClick(inputValidationFirstNameTutor, step1);
+  validateOnButtonClick(inputValidationSecondNameTutor, step1);
+
 
   //start function shake
   function shakeOnInvalid(input) {
