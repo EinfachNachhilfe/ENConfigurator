f12b880a8fbf96dbf8249f45f513179b7792adc8
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-08-22 12:10:44 +0200

    Update and rename request-customer_step30.js to request-customer_step31.js

--- request-customer_step31.js
+++ request-customer_step31.js
@@ -648,10 +648,11 @@ var isAllFieldsFilled4 = false;
 
 document.getElementById("coupon-code").oninput = function() {
     var input = this.value;
-    var pattern = /lernen2023/i;
+    var patternLernen = /lernen2023/i;
+    var patternHerbst = /herbst4/i;
     var messageElement = document.getElementById("message_coupon-code");
 
-    if (pattern.test(input)) {
+    if (patternLernen.test(input) || patternHerbst.test(input)) {
         this.style.borderColor = "green";
         this.style.borderWidth = '1.5px';
         messageElement.textContent = "Gutscheincode akzeptiert. Sie erhalten 2 x 90 Minuten kostenlose Nachhilfe!";
