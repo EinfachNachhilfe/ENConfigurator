c30318eb6554dd8bedf7c3802b6c483947885898
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-10-04 18:29:16 +0200

    Update and rename testtest55.js to testtest56.js

--- testtest56.js
+++ testtest56.js
@@ -165,37 +165,11 @@ inputElement.addEventListener("change", function() {
 }
 });
 
-
-submitBtn.addEventListener('click', function() {
-  if (isStepVisible(step) && nextBtn.classList.contains('disabled')) { 
-    if (inputElement.hasAttribute('required') && inputElement.value.trim() === '') {
-        errorMessageElement.innerHTML = 'Dieses Feld muss ausgefüllt werden.';
-        errorMessageElement.style.display = 'block';
-        inputElement.style.borderColor = '#9e367a'; 
-        inputElement.style.borderWidth = '1.5px';
-        validImage.style.display = 'none';
-        inValidImage.style.display = 'block';
-        shakeOnInvalid(inputElement);
-    }
+ if (nextBtn) {
+    nextBtn.removeEventListener('click', () => validateCurrentTab(step1Rt3ob)); // Verhindern Sie das mehrfache Anhängen von Event-Listenern
+    nextBtn.addEventListener('click', () => validateCurrentTab(step1Rt3ob));
 }
-});
-
-if (nextBtn) {
-    nextBtn.addEventListener('click', function() {
-    if (isStepVisible(step) && nextBtn.classList.contains('disabled')) { 
-        if (inputElement.hasAttribute('required') && inputElement.value.trim() === '') {
-            errorMessageElement.innerHTML = 'Dieses Feld muss ausgefüllt werden.';
-            errorMessageElement.style.display = 'block';
-            inputElement.style.borderColor = '#9e367a'; 
-            inputElement.style.borderWidth = '1.5px';
-            validImage.style.display = 'none';
-            inValidImage.style.display = 'block';
-            shakeOnInvalid(inputElement);
-        }
-    }
-    });
-
-}
+ 
 }
 
 //start const registration 3 or better
@@ -221,6 +195,21 @@ applyValidation(becomeAttentiveCustomerRt3ob, step4Rt3ob,'Dieses Feld muss ausge
 applyValidation(messageCustomerRt3ob, step4Rt3ob,'Dieses Feld muss ausgefüllt werden.', 'Ungültige Eingabe.');
 //end const registration 3 or better
 
+// Entfernen Sie diesen Teil aus der applyValidation-Funktion
+function validateCurrentTab(step) {
+    if (isStepVisible(step)) {
+        const inputs = step.querySelectorAll("input[required]");
+        for (let i = 0; i < inputs.length; i++) {
+            if (inputs[i].value.trim() === '') {
+                // Zeigen Sie Fehlermeldungen an, setzen Sie die Ränder usw.
+                // ... (Ihr vorhandener Code zur Anzeige von Fehlermeldungen)
+            } else if (!inputs[i].checkValidity()) {
+                // Zeigen Sie Fehlermeldungen an, wenn das Eingabefeld ungültig ist
+                // ... (Ihr vorhandener Code zur Anzeige von Fehlermeldungen)
+            }
+        }
+    }
+}
 
 //start function shake
 function shakeOnInvalid(input) {
