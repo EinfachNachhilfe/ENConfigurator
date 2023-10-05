9a0fbbee02f97c3c76728429e45253da7c0319b3
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-10-05 13:04:47 +0200

    Update and rename testtest61.js to testtest62.js

--- testtest62.js
+++ testtest62.js
@@ -162,21 +162,6 @@ inputElement.addEventListener("change", function() {
     }
 });
 
-
-submitBtn.addEventListener('click', function() {
-  if (isStepVisible(step) && nextBtn.classList.contains('disabled')) { 
-    if (inputElement.hasAttribute('required') && inputElement.value.trim() === '') {
-        errorMessageElement.innerHTML = 'Dieses Feld muss ausgefüllt werden.';
-        errorMessageElement.style.display = 'block';
-        inputElement.style.borderColor = '#9e367a'; 
-        inputElement.style.borderWidth = '1.5px';
-        validImage.style.display = 'none';
-        inValidImage.style.display = 'block';
-        shakeOnInvalid(inputElement);
-    }
-}
-});
-
 if (nextBtn) {
     nextBtn.addEventListener('click', function() {
     if (isStepVisible(step) && nextBtn.classList.contains('disabled')) { 
