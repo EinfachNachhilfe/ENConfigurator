f6f6d0364257e2d8d460b063a03274e1e1bdfe77
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-10-10 15:02:21 +0200

    Update and rename testtest283.js to testtest284.js

--- testtest284.js
+++ testtest284.js
@@ -3,18 +3,10 @@
         return;
     }
 
-//start general
-const nextBtn = document.querySelector('#nextBtn');
-const prevBtn = document.querySelector('#prevBtn');
-const submitBtn = document.querySelector('#submitBtn');
-const allInputs = document.querySelectorAll('input, select, textarea');
-let validationElements = {};
-//end general
-
-    document.addEventListener("DOMContentLoaded", function() {
-
+document.addEventListener("DOMContentLoaded", function() {
     // Beim Laden der Seite: Eingaben aus dem Local Storage wiederherstellen
-  allInputs.forEach(input => {
+    const allInputs = document.querySelectorAll("input, select");
+    allInputs.forEach(input => {
         let savedValue = localStorage.getItem(input.id);
         if (savedValue) {
             input.value = savedValue;
@@ -35,9 +27,18 @@ let validationElements = {};
             });
         });
     }
-
-    
 });
+
+
+
+
+//start general
+const nextBtn = document.querySelector('#nextBtn');
+const prevBtn = document.querySelector('#prevBtn');
+const submitBtn = document.querySelector('#submitBtn');
+const allInputs = document.querySelectorAll('input, select, textarea');
+let validationElements = {};
+//end general
 
 //start const registration 3 or better
 const zipCodeTeachingLocation = document.getElementById('zip-code_teaching-location');
@@ -303,6 +304,15 @@ allRadioButtons.forEach(radio => {
         checkRadioErrorStatus(group);
     });
 });
+
+
+
+
+
+
+
+
+
 
 
 submitBtn.addEventListener('click', function() {
