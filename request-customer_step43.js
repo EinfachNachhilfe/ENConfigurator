880e7bd39915a3ca8df7bfa949619fee8fcab61f
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2023-10-19 16:58:26 +0200

    Update and rename request-customer_step42.js to request-customer_step43.js

--- request-customer_step43.js
+++ request-customer_step43.js
@@ -656,16 +656,11 @@ var isAllFieldsFilled4 = false;
 
 
 //remove empty input
-document.addEventListener('submit', function(e) {
-    if (e.target.name === 'request_customer') {
-        var inputs = e.target.querySelectorAll('input, textarea, select');
-        inputs.forEach(function(input) {
-            if (!input.value) {
-                input.disabled = true;
-            }
-        });
-    }
+document.getElementById("myForm").addEventListener("submit", function(event){
+    event.preventDefault();
+    customSubmit();
 });
+
 
 
 
