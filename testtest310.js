be3f802501a4824bb963393b478488bcf9d93b23
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2024-01-17 12:09:00 +0100

    Update and rename testtest309.js to testtest310.js

--- testtest310.js
+++ testtest310.js
@@ -505,6 +505,13 @@ function validateForm() {
         }
     }
 
+    if (regForm && regForm.id === 'configurator' && ![5, 6].includes(currentTab)) {
+    const hasClicked = Array.from(formItems[currentTab].querySelectorAll('.custom-input-clicked')).length > 0;
+    if (!hasClicked) {
+        valid = false;
+    }
+}
+
 
         // Validierung für Radio-Buttons
         const radios = formItems[currentTab].querySelectorAll("input[type='radio'][required]");
