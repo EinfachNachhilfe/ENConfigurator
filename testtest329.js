4dec531df6e46c5fb201c566e14c36bf1e1ec0f9
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2024-01-17 15:07:16 +0100

    Update and rename testtest328.js to testtest329.js

--- testtest329.js
+++ testtest329.js
@@ -590,18 +590,12 @@ if (valid) {
         }
 
 
-const configuratorForm = document.getElementById('configurator');
-if (configuratorForm && ![4, 5, 6].includes(currentTab)) {
-    const hasClicked = Array.from(formItems[currentTab].querySelectorAll('.custom-input-clicked')).length > 0;
-    if (nextBtn) {
-        if (hasClicked) {
-            nextBtn.classList.remove("disabled");
-        } else {
-            nextBtn.classList.add("disabled");
+    if (configuratorForm && ![4, 5, 6].includes(currentTab)) {
+        const hasClicked = Array.from(formItems[currentTab].querySelectorAll('.custom-input-clicked')).length > 0;
+        if (!hasClicked) {
+            valid = false;
         }
     }
-}
-
 
 
 
