d9096a88401f081c219708009bb6aa0751ef6857
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2024-01-17 15:09:01 +0100

    Update and rename testtest329.js to testtest330.js

--- testtest330.js
+++ testtest330.js
@@ -589,7 +589,7 @@ if (valid) {
         }
         }
 
-
+const configuratorForm = document.getElementById('configurator');
     if (configuratorForm && ![4, 5, 6].includes(currentTab)) {
         const hasClicked = Array.from(formItems[currentTab].querySelectorAll('.custom-input-clicked')).length > 0;
         if (!hasClicked) {
