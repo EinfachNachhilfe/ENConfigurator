4173eb64eb626e4a0918cbf7a3783e3d50878dad
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2024-01-23 14:16:00 +0100

    Update and rename testtest460.js to testtest461.js

--- testtest461.js
+++ testtest461.js
@@ -111,6 +111,7 @@ function manageSelection(elements, maxSelected, selectionClass) {
                 if (selectedElements.length >= maxSelected) {
                     selectedElements[0].classList.remove(selectionClass);
                     selectedElements.shift();
+                    removeCodeGenerator(area, codeGenerator);
                 }
                 selectedElements.push(element);
                 element.classList.add(selectionClass);
