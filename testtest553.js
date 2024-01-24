e07d6e60fcde1ee4045c6f3802cefe5ab7ceb8cd
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2024-01-24 18:28:15 +0100

    Update and rename testtest552.js to testtest553.js

--- testtest553.js
+++ testtest553.js
@@ -218,7 +218,7 @@ function createInputField(elementOrElements, additionalLessonCost,additionalLess
             if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
                 const targetElement = mutation.target;
                 handleClassChange(targetElement, additionalLessonCost,additionalLessonTutorSalary, codeGenerator, defaultValue, area);
-                manageSelection(targetElements, area, codeGenerator)
+                manageSelection(elements, area, codeGenerator)
                 updateCodeGenerator(area, codeGenerator)
                 removeCodeGenerator(area, codeGenerator)
 }
