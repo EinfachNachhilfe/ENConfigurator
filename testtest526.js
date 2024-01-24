0cbe6cbbf308c1a3d09d45cb770affdd6f28761a
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2024-01-24 15:00:18 +0100

    Update and rename testtest525.js to testtest526.js

--- testtest526.js
+++ testtest526.js
@@ -209,6 +209,7 @@ function createInputField(elementOrElements, additionalLessonCost,additionalLess
             if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
                 const targetElement = mutation.target;
                 handleClassChange(targetElement, additionalLessonCost,additionalLessonTutorSalary, codeGenerator, defaultValue, area);
+                manageSelection(elements, maxSelected, selectionClass, area, codeToAdd, codeToRemove)
             }
         });
     });
