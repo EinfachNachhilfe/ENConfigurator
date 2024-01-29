85924b3ebb62fc3a701897ec7802b514530ce817
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2024-01-29 14:26:49 +0100

    Update and rename testtest592.js to testtest593.js

--- testtest593.js
+++ testtest593.js
@@ -151,18 +151,26 @@ function manageSelection(elements, maxSelected, selectionClass) {
 function makeExclusivePair(id1, id2, disabledClass) {
     const element1 = document.getElementById(id1);
     const element2 = document.getElementById(id2);
+    let firstClick1 = true;
+    let firstClick2 = true;
 
     if (element1 && element2) {
         element1.addEventListener('click', () => {
-            const isDisabled = element1.classList.contains(disabledClass);
-            element1.classList.toggle(disabledClass, !isDisabled);
-            element2.classList.add(disabledClass);
+            if (firstClick1) {
+                element2.classList.add(disabledClass);
+            } else {
+                element2.classList.remove(disabledClass);
+            }
+            firstClick1 = !firstClick1;
         });
 
         element2.addEventListener('click', () => {
-            const isDisabled = element2.classList.contains(disabledClass);
-            element2.classList.toggle(disabledClass, !isDisabled);
-            element1.classList.add(disabledClass);
+            if (firstClick2) {
+                element1.classList.add(disabledClass);
+            } else {
+                element1.classList.remove(disabledClass);
+            }
+            firstClick2 = !firstClick2;
         });
     }
 }
