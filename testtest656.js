87c9041d47213ef30059ca8b5b1f9688a2b5a5ad
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2024-02-03 12:59:10 +0100

    Update and rename testtest655.js to testtest656.js

--- testtest656.js
+++ testtest656.js
@@ -150,7 +150,40 @@ manageSelection(customRadioInputContract, 1, 'custom-input-clicked', 'disabled')
 manageSelection(customCheckboxInputTutor, 5, 'custom-input-clicked', 'disabled');
 manageSelection(customCheckboxInputOther, 2, 'custom-input-clicked', 'disabled');
 
-        
+ // Globales Objekt, um den Zustand des zuletzt aktivierten Elements zu speichern
+let lastClicked = null;
+
+function makeExclusivePair(id1, id2, disabledClass) {
+    const element1 = document.getElementById(id1);
+    const element2 = document.getElementById(id2);
+    let firstClick1 = true;
+    let firstClick2 = true;
+
+    if (element1 && element2) {
+        element1.addEventListener('click', () => {
+            if (firstClick1) {
+                element2.classList.add(disabledClass);
+            } else {
+                element2.classList.remove(disabledClass);
+            }
+            firstClick1 = !firstClick1;
+        });
+
+        element2.addEventListener('click', () => {
+            if (firstClick2) {
+                element1.classList.add(disabledClass);
+            } else {
+                element1.classList.remove(disabledClass);
+            }
+            firstClick2 = !firstClick2;
+        });
+    }
+}
+    
+makeExclusivePair('addOnPremiumTutor', 'addOnExperiencedTutor', 'disabled');
+makeExclusivePair('addOnFemale', 'addOnMale', 'disabled');
+
+    
 let totalLessonPrice = 20;
 let tutorSalary = 12;
     
