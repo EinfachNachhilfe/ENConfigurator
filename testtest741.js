7529a76f1bb1b5f0cd407fbb0465b75d8a88e437
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2024-02-07 14:57:23 +0100

    Update and rename testtest740.js to testtest741.js

--- testtest741.js
+++ testtest741.js
@@ -292,11 +292,11 @@ function calculateTotalCost() {
     
     if (unitMiddle.classList.contains('custom-input-clicked')) {
         multiplierUnit = 4;
-        totalLessonPrice -= valueUnitSmall;
+
 
     } else if (unitLarge.classList.contains('custom-input-clicked')) {
         multiplierUnit = 6;
-        totalLessonPrice -= valueUnitSmall;
+
     }
 
     if (contractSmall.classList.contains('custom-input-clicked')) {
