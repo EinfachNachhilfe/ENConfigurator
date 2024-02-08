1ffe425501975771f1fdf3fd8d28962f8e2685ec
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2024-02-08 14:31:48 +0100

    Update and rename testtest765.js to testtest766.js

--- testtest766.js
+++ testtest766.js
@@ -368,8 +368,8 @@ document.querySelector('.send-totalMonthPrice').value = totalMonthPrice.toFixed(
     let discountUnitMiddle = discountUnit + valueUnitMiddle;
     let discountUnitLarge = discountUnit + valueUnitLarge;
 
-    let totalDiscountUnitMiddle = discountUnitSmall * (discountUnitMiddle/100);
-    let totalDiscountUnitLarge = discountUnitSmall * (discountUnitLarge/100);
+    let totalDiscountUnitMiddle = discountUnitSmall * ((discountUnitSmall - discountUnitMiddle) /100);
+    let totalDiscountUnitLarge = discountUnitSmall * ((discountUnitSmall -discountUnitLarge) /100);
 
     textdiscountUnitMiddle.textContent = totalDiscountUnitMiddle.toFixed(1).replace('.', ','); 
     textdiscountUnitLarge.textContent = totalDiscountUnitLarge.toFixed(1).replace('.', ','); 
