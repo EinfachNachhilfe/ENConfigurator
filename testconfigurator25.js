6a38bb889bfe59fc816e7885eb333df3f660de94
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2024-02-17 18:19:49 +0100

    Update and rename testconfigurator24.js to testconfigurator25.js

--- testconfigurator25.js
+++ testconfigurator25.js
@@ -571,7 +571,7 @@
 		let baseCode = "B-0A0A0A-0A-0A-0A-0A0A0A0A0A0A0A0A0A0A0A0A";
 		let baseTutorSalary = 12;
 
-		conssole.log(baseCode);
+		console.log(baseCode);
 
 
 		function updateSubject(element, lessonValue, letterValue) {
