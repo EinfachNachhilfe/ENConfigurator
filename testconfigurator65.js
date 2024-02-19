13527f21ec304aef8ed077bec2242d31d7b4c4c7
EinfachNachhilfe <126563581+EinfachNachhilfe@users.noreply.github.com>
2024-02-19 14:37:54 +0100

    Update and rename testconfigurator64.js to testconfigurator65.js

--- testconfigurator65.js
+++ testconfigurator65.js
@@ -841,23 +841,23 @@ function updateAddOn(element, lessonValue, letterValue, tutorSalaryValue) {
 
 	
 
-		let totalMonthPrice = baseLessonPrice * multiplierUnit * 4.3333333333;
+		//let totalMonthPrice = baseLessonPrice * multiplierUnit * 4.3333333333;
     
 		//calculation months salary tutor
 		let monthlyTutorCost = baseTutorSalary * multiplierUnit * 4.3333333333;
 		
 		//calculation LTV
-		let valueTotalLtv = (totalMonthPrice - monthlyTutorCost) * multiplierContract + setUpFee;
+		//let valueTotalLtv = (totalMonthPrice - monthlyTutorCost) * multiplierContract + setUpFee;
 	
-		 let totalTutorSalary = (baseTutorSalary / 45) * 60;
+		// let totalTutorSalary = (baseTutorSalary / 45) * 60;
 
 		 textTotalMonthPrice.textContent = totalMonthPrice.toFixed(2).replace('.', ',');
  
 		//send value to form
 		document.querySelector('.send-lessonPrice').value = baseLessonPrice.toFixed(2);
-		document.querySelector('.send-totalLtv').value = valueTotalLtv.toFixed(2);
-		document.querySelector('.send-totalTutorSalary').value = baseTutorSalary.toFixed(2);
-		document.querySelector('.send-totalMonthPrice').value = totalMonthPrice.toFixed(2);
+		//document.querySelector('.send-totalLtv').value = valueTotalLtv.toFixed(2);
+		//document.querySelector('.send-totalTutorSalary').value = baseTutorSalary.toFixed(2);
+		//document.querySelector('.send-totalMonthPrice').value = totalMonthPrice.toFixed(2);
 		document.querySelector('.send-codeGenerator').value = baseCode;
 	
 	}
